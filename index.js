e05b4690f0fbdc218b5d8d26bf9d0fdc5f8e64af
Dmitrii Art <artuhovdmitrii@gmail.com>
2021-06-29 18:57:30 +0300

    fix

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ const startCluster = async () => {
   });
 
   await cluster.task(async ({ page, data: url }) => {
-    await processForm(page, url);
+    // await processForm(page, url);
+		await processSecondForm(page, url);
   });
 
 	for (let i = 0; i < totalFormSubmissions; i++) {
@@ -130,6 +131,86 @@ const processForm = async (page, url) => {
 	
 	// Вы находитесь в кафе с друзьями, и Вам нужно срочно перевести небольшую сумму денег родственнику. Какой из предложенных вариантов действий следует выбрать, как самый безопасный (radio)
 	await selectors[getRandomInt(58, 59)].click();
+
+
+	// Submit form
+	await page.click(".appsMaterialWizButtonPaperbuttonFocusOverlay");
+	await page.waitForNavigation();
+
+	const submissionPage = await page.url();
+	
+	if (!submissionPage.includes("formResponse")) {
+		throw new Error('This worker was not able to submit a form!');
+	}
+}
+
+const processSecondForm = async (page, url) => {
+	// Opening Form
+	await page.goto(url, {
+		waitUntil: 'networkidle2'
+	});
+
+
+
+	// MCQ and Checkbox Questions
+	const selectors = await page.$$('.docssharedWizToggleLabeledLabelWrapper');
+		
+	// Укажите Ваш пол (radio)
+	await selectors[getRandomInt(0, 1)].click();
+
+	// Укажите Ваш район проживания (dropdown menu)
+	await page.click('.quantumWizMenuPaperselectOption');
+	await page.waitForTimeout(200);
+	const dropDownItems = await page.$$('.quantumWizMenuPaperselectPopup > .quantumWizMenuPaperselectOption');
+	await dropDownItems[1].click();
+	await page.waitForTimeout(500);
+
+	// Укажите Ваш возраст (radio)
+	await selectors[getRandomInt(2, 6)].click();
+	
+	// Укажите Ваш статус (radio)
+	await selectors[getRandomInt(7, 9)].click();
+
+	// По Вашему мнению, представляет ли в настоящее время идеология терроризма и насильственного экстремизма реальную угрозу для государства и российского общества (radio)
+	await selectors[getRandomInt(10, 12)].click();
+
+	// Как Вы оцениваете в настоящее время вероятность проведения террористических актов в месте Вашего проживания (городе, сельском населенном пункте) (radio)
+	await selectors[getRandomInt(13, 16)].click();
+
+	// Чувствуете ли Вы себя защищенным от террористических актов (radio)
+	await selectors[getRandomInt(17, 19)].click();
+	
+	// Какие чувства Вы испытываете по отношению к террористам (checkboxes)
+	await selectors[getRandomInt(20, 23)].click();
+	
+	// Как Вы полагаете, кем является террорист в современной России (checkboxes)
+	await selectors[getRandomInt(25, 28)].click();
+
+	// В чем Вы видите причину существования террористических угроз в Российской Федерации? (checkboxes)
+	await selectors[getRandomInt(30, 32)].click();
+
+	// Как за последний год изменилось Ваше отношение к деятельности силовых структур в сфере борьбы с терроризмом (radio)
+	await selectors[getRandomInt(34, 37)].click();
+
+	// Как Вы считаете, должны ли граждане России помогать спецслужбам в борьбе с терроризмом (radio)
+	await selectors[getRandomInt(38, 40)].click();
+
+	// Как Вы считаете, что необходимо предпринять для повышения эффективности борьбы с терроризмом (checkboxes)
+	await selectors[getRandomInt(41, 45)].click();
+
+	// По Вашему мнению, каким образом происходит наиболее активное распространение идей терроризма и насильственного экстремизма (checkboxes)
+	await selectors[getRandomInt(47, 51)].click();
+
+	// Как Вы считаете, применение каких из приведенных ниже мер дадут наибольший эффект в борьбе с распространением идеологии терроризма и насильственного экстремизма (checkboxes)
+	await selectors[getRandomInt(53, 58)].click();
+
+	// Если бы в Вашем районе проходил семинар «Как не стать жертвой вербовки?», Вы посетили бы данное мероприятие?  (radio)
+	await selectors[getRandomInt(60, 61)].click();
+
+	// Как Вы считаете, социальная реклама, призывающая к совместной борьбе с терроризмом, может повлиять на Ваше поведение (radio)
+	await selectors[getRandomInt(62, 64)].click();
+
+
 
 
 	// Submit form
